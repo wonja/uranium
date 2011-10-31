1931f752e84355bb7eec922336397ef3246d9381
Sean Jezewski <seanwjezewski@gmail.com>
2011-10-31 00:40:06 -0700

    Have pretty and minified versions working

--- site/controllers.rb
+++ site/controllers.rb
@@ -21,7 +21,14 @@ module Uranium
                        "uranium_mixins.js"
                       ]
 
-      def bundle(params, bundler_klass, browser='webkit')
+
+      # TODO: Update Ur so I can actually obfuscate instead of just do simple optimizations
+      @@bundlers = {
+        "pretty" => Fusion::Quick,
+        "minified" => Fusion::Optimized 
+      }
+
+      def bundle(params, bundler_mode='pretty', browser='webkit')
         content_type 'text/plain'
 
         if params.keys.size == 0 || params.keys.include?("all")
@@ -29,12 +36,13 @@ module Uranium
         else
           widgets = []
 
+          # NOTE : Should I throw an error if a widget is specified but not found?
           params.each do |widget, flag|
             widget_file = File.join(@@lib_path, "#{widget}.js")
             widgets << widget if File.exist?( widget_file )
           end
 
-          build_key = get_key(widgets)
+          build_key = get_key(widgets, bundler_mode)
 
           existing_bundle = fetch_bundle(build_key)
           return existing_bundle if existing_bundle
@@ -52,8 +60,9 @@ module Uranium
 
 
           # I dont think this is threadsafe ...
+          # TODO: Update fusion so that each instance has a separate config
           Fusion.configure({:bundle_configs => [this_bundle], :project_path => @@lib_path})
-          bundler = bundler_klass.new
+          bundler = @@bundlers[bundler_mode].new
           js = bundler.run.first
           
           save_bundle(build_key, js)
@@ -64,19 +73,20 @@ module Uranium
 
       get "/uranium.js" do 
         # Is it going to be performance-prohibitive to run GCC?
-        bundle(params, Fusion::Optimized)
+        bundle(params, "minified")
       end
 
       get "/uranium-pretty.js" do
         puts params
         puts headers
-        bundle(params, Fusion::Quick)
+        bundle(params)
       end
 
       private
 
-      def get_key(widgets)
-        key = "xui-#{xui_version}---"
+      def get_key(widgets, bundler_mode)
+        key = "#{bundler_mode}-"
+        key << "xui-#{xui_version}---"
         key << widgets.sort.join("--")
         key
       end
